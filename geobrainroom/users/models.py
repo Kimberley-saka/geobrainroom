857fd194a89f38ab7cebb3f0cc48f031c2c7dd61
Kimberley saka <kimberleysaka@gmail.com>
2023-10-15 13:50:22 +0200

    updated models

--- geobrainroom/users/models.py
+++ geobrainroom/users/models.py
@@ -5,7 +5,6 @@ class Users(models.Model):
     """
     user db model definition
     """
-    id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=250, null=False, default='username')
     email = models.CharField(max_length=250, null=False, default='example@gmail.com')
     password = models.CharField(max_length=250, null=False)
