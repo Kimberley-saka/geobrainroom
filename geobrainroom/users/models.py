2b573b0a8861692c980852c6742b1156fc7dc5dc
Kimberley saka <kimberleysaka@gmail.com>
2023-10-24 13:02:57 +0200

    added create superuser method

--- geobrainroom/users/models.py
+++ geobrainroom/users/models.py
@@ -13,6 +13,27 @@ class UserManager(BaseUserManager):
         """
         return self.get(username=username)
     
+    def create_user(self, email, password=None, **kwargs):
+        """
+        Create and return a regular user with an email and password
+        """
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+        # Create and return a superuser with an email and password
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **kwargs)
+    
 class Users(models.Model):
     """
     user db model definition
